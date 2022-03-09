601bb505b8129846334fe55b24b42013fe79fa04
FedeTor <fedeetorancio@gmail.com>
2022-03-09 19:22:08 -0300

    commit inicial

---
+++ PaginaAbove/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PaginaAbove.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaginaAbove.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult EnviarDatosContacto(EnvioDatos enviodatos)
+        {
+            return RedirectToAction("EnviarDatosContacto");
+        }
+
+        public IActionResult EnviarDatosContacto()
+        {
+            return View();
+        }
+    }
+
+
+}
---
+++ PaginaAbove/Servicios/ServicioEnvioDatos.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using PaginaAbove.Models;
+using System.Threading.Tasks;
+
+namespace PaginaAbove.Servicios
+{
+    //public class ServicioEnvioDatos : IServicioEnviarDatos
+    //{
+    //    private readonly IConfiguration _configuration;
+    //    public ServicioEnvioDatos(IConfiguration configuration)
+    //    {
+    //        _configuration = configuration;            
+    //    }
+
+    //    private SqlConnection con;
+    //    private void Conectar()
+    //    {
+    //        string constr = _configuration.GetConnectionString("DefaultConnection");
+    //        con = new SqlConnection(constr);
+    //    }
+
+    //    public Task<EnvioDatos> EnviarDatosAsync(EnvioDatos enviodatos)
+    //    {
+    //        throw new System.NotImplementedException();
+    //    }
+
+    //    public int EnvioDatos(EnvioDatos enviodatos)
+    //    {
+    //        Conectar();
+    //        SqlCommand comando = new SqlCommand("insert into articulos(codigo,descripcion,precio) values (@codigo,@descripcion,@precio)", con);
+    //        comando.Parameters.Add("@codigo", SqlDbType.Int);
+    //        comando.Parameters.Add("@descripcion", SqlDbType.VarChar);
+    //        comando.Parameters.Add("@precio", SqlDbType.Float);
+    //        comando.Parameters["@codigo"].Value = art.Codigo;
+    //        comando.Parameters["@descripcion"].Value = art.Descripcion;
+    //        comando.Parameters["@precio"].Value = art.Precio;
+    //        con.Open();
+    //        int i = comando.ExecuteNonQuery();
+    //        con.Close();
+    //        return i;
+    //    }
+    //}
+}
