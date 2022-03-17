dd4b2de8ebef3727e7b911d49c59b2cc445b7a26
FedeTor <fedeetorancio@gmail.com>
2022-03-17 19:57:57 -0300

    codificando la recepcion de mail

--- PaginaAbove/Controllers/HomeController.cs
+++ PaginaAbove/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PaginaAbove.Models;
+using PaginaAbove.Servicios;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,6 +12,11 @@ namespace PaginaAbove.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEnvioMail _enviomail;
+        public HomeController(IEnvioMail enviomail)
+        {
+            _enviomail = enviomail;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,6 +30,38 @@ namespace PaginaAbove.Controllers
         }
 
         public IActionResult EnviarDatosContacto()
+        {
+            return View();
+        }
+
+
+        [HttpPost("EnvioMail")]
+        public async Task<IActionResult> Send([FromForm] RequestEnvioMail request)
+        {
+            var verdadero = true;       
+            try
+            {
+                var envio = new EnvioMail()
+                {
+                    Body = request.mensaje,
+                    Subject = request.Email,
+                    ToEmail = "fedeetorancio@gmail.com"
+                };
+                await _enviomail.SendEmailAsync(envio);
+                TempData["ok"]  = verdadero;
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                verdadero = false;
+                TempData["ok"] = verdadero;
+                throw ex;
+            }
+
+        }
+
+        public async Task<IActionResult> EnvioMailCorrecto()
         {
             return View();
         }
